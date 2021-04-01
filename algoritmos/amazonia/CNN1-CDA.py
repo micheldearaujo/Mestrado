c8a268a315653cf81d921267fb3cd0687afbad05
Michel de Araujo <78417934+micheldearaujo@users.noreply.github.com>
2021-04-01 13:37:44 -0300

    atualizacao

--- algoritmos/amazonia/CNN1-CDA.py
+++ algoritmos/amazonia/CNN1-CDA.py
@@ -18,7 +18,8 @@ from datetime import timedelta
 start_time = time.monotonic()
 
 # Definindo o caminho dos diretorios
-base_dir = 'D:/michel/data/amazonia/kaggle'
+#base_dir = '/home/michel/PycharmProjects/data/amazonia' # Ubuntu
+base_dir = 'D:/michel/data/amazonia/kaggle' # Windows
 train_dir = os.path.join(base_dir, 'train-jpg')
 test_dir = os.path.join(base_dir, 'test-jpg')
 train_fnames = os.listdir(train_dir)
