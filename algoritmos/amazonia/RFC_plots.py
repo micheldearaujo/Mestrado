e451822f652b2cd191ff91fd8043eb20f1859cf1
Michel de Araujo <78417934+micheldearaujo@users.noreply.github.com>
2021-04-12 15:08:35 -0300

    fiz graficos

--- algoritmos/amazonia/RFC_plots.py
+++ algoritmos/amazonia/RFC_plots.py
@@ -6,8 +6,8 @@ import os
 from tensorflow.keras.optimizers import SGD
 
 # Definindo o caminho dos diretorios
-base_dir = '/home/michel/data/amazonia/kaggle' # Ubuntu
-#base_dir = 'D:/michel/data/amazonia/kaggle'
+#base_dir = '/home/michel/data/amazonia/kaggle' # Ubuntu
+base_dir = 'D:/michel/data/amazonia/kaggle'
 train_dir = os.path.join(base_dir, 'train-jpg')
 test_dir = os.path.join(base_dir, 'test-jpg')
 train_fnames = os.listdir(train_dir)
@@ -15,9 +15,11 @@ test_fnames = os.listdir(test_dir)
 
 
 scores = pd.read_csv(base_dir+'/'+'RFC_Scores_ALL.csv')
-#sns.jointplot(data=scores, x='Avg Recall', y='Avg Precision', hue='Target_size',xlim=(0.2,1.1), ylim=(0.2,1.1))
-sns.jointplot(data=scores, x='Avg Recall', y='Avg Precision', hue='n_trees', palette='rocket')
-sns.jointplot(data=scores, x='Avg Recall', y='Avg Precision', hue='Target_size',xlim=(0.8,1), ylim=(0.8,1), palette='flare')
-
+sns.set_style('whitegrid')
+fig = sns.FacetGrid(data = scores, col='n_trees', hue='Target_size', palette = 'icefire')
+fig.map(sns.scatterplot, 'Avg Recall', 'Avg Precision', s= 100)
+# sns.jointplot(data=scores, x='Avg Recall', y='Avg Precision', hue='n_trees', palette='icefire', ax=axs[0])
+# sns.jointplot(data=scores, x='Avg Recall', y='Avg Precision', hue='Target_size', palette='icefire', ax=axs[1])
+fig.add_legend()
 
 plt.show()
